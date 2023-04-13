cb08fc898f0dba38cc7bb7eebeab9850ecc7037b
eugenebalts <norcobeatz@gmail.com>
2023-04-13 17:45:47 +0200

    dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,16 +2,25 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 function createDreamTeam(array) {
   let result = [];
-  for (let i = 0; i < array.length; i++) {
-    if (typeof array[i] == 'string') {
-      result.push(array[i].slice(0,1).toUpperCase())
-    } 
-  }
-  return result.sort()
+  if (Array.isArray(array)) {
+    // console.log('yes')
+    for (let i = 0; i < array.length; i++) {
+      if (typeof array[i] === 'string') {
+        let word = array[i];
+        let finalWord = word.trim()
+        result.push(finalWord[0].toUpperCase())
+      }
+    }
+    result.sort()
+    return result.join('')
+  } else return false
 }
-
 module.exports = {
   createDreamTeam
 };
 
-// console.log(createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max', true]));
+// console.log(createDreamTeam(['Matt ADAMS', 'Ann', 'Dmitry', 'Max', true]));
+
+// let word = 'Matt ADAMS';
+
+// console.log(word.split(''))
