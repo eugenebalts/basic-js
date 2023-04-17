ad506ab5c982ec6b742724fda0edfe34f3ccddd4
eugenebalts <norcobeatz@gmail.com>
2023-04-17 21:15:42 +0200

    basic

--- src/transform-array.js
+++ src/transform-array.js
@@ -17,18 +17,36 @@ function transform(arr) {
   if(!Array.isArray(arr)) {
     throw new Error("'arr' parameter must be an instance of the Array!");
   }
+  if (arr.length === 0) {
+    return arr;
+  }
   let result = [];
-  let command = {
-    '--discard-next' : i += 1,
-    '--discard-prev' : arr[i - 2] !== "--discard-next" ? result.pop() : '',
-    '--double-next': result.push(arr[i + 1]),
-    '--double-prev': result.push(arr[i - 1])
-  }
+
   for (let i = 0; i < arr.length; i++) {
-    
+    // let command = {
+    //   '--discard-next' : `${i += 1}`,
+    //   '--discard-prev' : `${arr[i - 1] ? result.splice(arr[i - 1], 1) : ''}`,
+    //   '--double-next': `${result.push(arr[i + 1])}`,
+    //   '--double-prev': `${result.push(arr[i - 1])}`
+    // }
+
+    if (arr[i] == '--discard-next') {
+      i += 1
+    } else if (arr[i] == '--discard-prev') {
+      result.push(arr[i + 1])
+    } else if (arr[i] == '--double-next') {
+      result.push(arr[i + 1])
+    } else if (arr[i] == '--double-prev') {
+      arr[i - 1] ? result.push(arr[i - 1]) : false
+    } else {
+      result.push(arr[i]);
+    }
   }
+  return result
 }
 
 module.exports = {
   transform
 };
+
+console.log(transform(['--double-prev', 1, 2, 3]))
